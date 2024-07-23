07a2c08f2a6666daefb0f813721d1edbeac3412a
Ajay <nemaligaajayreddy80@gmail.com>
2024-07-23 11:45:41 +0530

    changed secretOrKey

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.TOKEN_SECRET ,
+      secretOrKey: "Secret",
      /*  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('Secret'), */
